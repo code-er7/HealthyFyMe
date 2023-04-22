02b15f948863a3f72bb3bc22124a5f8d9431bd4e
Chetan Rao <chetanrao2003@gmail.com>
2023-04-22 17:44:18 +0530

    added more User info

--- app.js
+++ app.js
@@ -9,25 +9,37 @@ app.set('view engine' , 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 mongoose.connect("mongodb://localhost:27017/healthyUsers");
 var counter = 0 ;
+var Userdata = null;
 const appUser = new mongoose.Schema({
+    name:String,
+    bmi:Number,
     email:String,
     password:String
 });
+
 const User =  new mongoose.model('user' , appUser);
 app.get("/" , function(req , res){
+    Userdata = null
     res.render("home");
 });
 app.get("/login" , function(req , res){
+    Userdata = null
     res.render("login" , {count: counter });
 });
 app.get("/register" , function(req , res){
     counter = 0 ;
+    Userdata = null
     res.render("register");
 });
 app.get("/website" ,function(req, res){
     counter = 0 ;
+    if(Userdata==null)res.redirect("/login");
+    else{
+    console.log(Userdata.name , Userdata.BMI);
     res.render("website");
-})
+    }
+    
+});
 app.post("/login" , function(req , res){
     var Uemail = req.body.username;
     var password = req.body.password;
@@ -36,6 +48,12 @@ app.post("/login" , function(req , res){
     async function datafinder(){
         try{
            const foundUser = await User.findOne({email:Uemail});
+           if(foundUser == null){
+            counter++;
+            res.render("login" , {count: counter });  
+           }
+           else{
+            Userdata = foundUser;
            if(foundUser.password==password){
             counter = 0 ;
             res.redirect("/website");
@@ -44,6 +62,7 @@ app.post("/login" , function(req , res){
                counter++;
                res.render("login" , {count: counter });  
            }
+           } 
         }
         catch(err){
             console.log(err);
@@ -55,10 +74,16 @@ app.post("/register" , function(req , res){
     counter = 0;
     var Uemail = req.body.username;
     var Upassword = req.body.password;
+    var uname = req.body.name;
+    var Uweight = req.body.weight;
+    var Uheight = req.body.height;
+    const BMI = Uweight/(Uheight*Uheight);
     registerinDB();
     async function registerinDB(){
         try{
           const newUser = User({
+            name:uname,
+            bmi:BMI,
             email:Uemail,
             password:Upassword
           });
